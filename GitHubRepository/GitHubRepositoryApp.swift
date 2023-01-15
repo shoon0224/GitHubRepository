ee5324f0e9d7f13bf036da10085f1f9818e164dc
이상훈 <>
2023-01-15 17:54:16 +0900

    Initial commit

---
+++ GitHubRepository/GitHubRepositoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoryApp.swift
+//  GitHubRepository
+//
+//  Created by 이상훈 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
